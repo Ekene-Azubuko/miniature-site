7f6885ffe21bd3dfd05786203303f2cdce7fea20
Ekene-Azubuko <117138463+Ekene-Azubuko@users.noreply.github.com>
2024-01-30 02:29:49 -0800

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,14 @@
+let na="Ekene"
+let ad="Jos"
+let age = 17
+let dob='20/09/2005'
+let city='Nigerian'
+let pob='Jos'
+let rel='Christianity'
+
+const states=["Plateau","Lagos","Edo","Rivers"]
+
+const Ekene={na,ad,age,dob,city,pob,rel};
+document.getElementById("demo").innerHTML=states;
+
+
